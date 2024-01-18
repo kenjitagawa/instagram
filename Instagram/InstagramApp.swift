6955437e5adb45a74a73906cedbe25c1f2d63792
Kenji Tagawa <kenji@tagawa.ca>
2024-01-18 18:51:46 -0500

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Kenji Tagawa on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
